6df567592c97ec62244d0a8bc38f833c1c0053df
GitSioke <relojeroloco@gmail.com>
2013-03-07 20:01:11 +0100

    asd

---
+++ src/remind/me/SpinnerRepeat.java
@@ -0,0 +1,21 @@
+package remind.me;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class SpinnerRepeat extends Activity implements OnItemSelectedListener{
+
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
