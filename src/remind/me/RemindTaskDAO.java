8dba54184ca1f108cc91cc2dd3e00876646ba2ac
GitSioke <relojeroloco@gmail.com>
2013-03-15 07:25:11 +0100

    DAO

---
+++ src/remind/me/RemindTaskDAO.java
@@ -0,0 +1,12 @@
+package remind.me;
+
+import java.util.ArrayList;
+
+public interface RemindTaskDAO {
+	public ArrayList<RemindTask> getTaskWithTag(String tag);
+	public ArrayList<String> getAllTags();
+	public ArrayList<RemindTask> getAllTasks();
+	public long insertNewTask(RemindTask task);
+	public int updateTask(RemindTask task);
+	public void updateTaskCompleted(RemindTask task);
+}
