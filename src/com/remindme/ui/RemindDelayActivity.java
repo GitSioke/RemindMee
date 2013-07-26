b421b2d6998c8c1cb0cf9a84fb0229aaeb27b35d
GitSioke <relojeroloco@gmail.com>
2013-07-26 16:52:24 +0200

    DelayActivity show new dialog with deayed options

---
+++ src/com/remindme/ui/RemindDelayActivity.java
@@ -0,0 +1,16 @@
+package com.remindme.ui;
+
+import com.remindme.ui.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RemindDelayActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result);
+	}
+
+}
